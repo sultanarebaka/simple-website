6862970928d190c948a326ae50d2c45b97a19e0b
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-05-14 19:37:20 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const Scolor = document.getElementById('S-color')
 
 const Svalues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
 const background = (document.body.style.background = '#DC0000')
+
+changeBtn.addEventListener('click', () => {
+    let SSymbol = '#'
+}
+)
