7c8e2815481c52497fac8eac5664d82bcbabf2f5
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-05-14 19:12:34 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 const changeBtn = document.getElementById('changeBtn')
 const Scolor = document.getElementById('S-color')
+
+const Svalues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+const background = (document.body.style.background = '#DC0000')
