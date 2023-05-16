0f5757499b58957cc48e646db4c4939a77be7962
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-05-16 09:01:20 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var color = ["#749F82"];
+var i = 0;
+document.querySelector("button").addEventListener("click", 
+function(){
+i = i < color.length ? ++i : 0;
+document.querySelector("body").style.background = color[i]
+})
