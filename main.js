340ce7047168d86e6b6a4af7b8e2f6e6d23abef0
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-05-16 22:27:26 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-var color = ["#749F82"];
+const colors = ["yellow"];
+const show = document.querySelector("#change");
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", function myFunction(){
+	let start = "#";
+	for(let i=0; i<1; i++){
+		start += colors[getRandom()];
+	
+	
+	}
+
+	show.textContent = start ;
+	document.body.style.background = start;
+});
+function getRandom(){
+	return Math.floor(Math.random() *colors.length)
+};
+
+
+
+var color = ["#FEFF86"];
 var i = 0;
 document.querySelector("button").addEventListener("click", 
 function(){
 i = i < color.length ? ++i : 0;
 document.querySelector("body").style.background = color[i]
-})
+});
 
-const colors = ["red","blue"];
-const show = document.querySelector("#change");
+
+
